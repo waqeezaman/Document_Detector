20b28d28fd757dead22d844b5d3cfe4e50571db5
Muhammad Khan <muhammad.khan-52@student.manchester.ac.uk>
2024-06-21 18:15:45 +0100

    cleaned up code, added contrast increase, need to refine variables, possibly create trackbars and look into more processing steps

--- document_scanner.py
+++ document_scanner.py
@@ -1,9 +1,9 @@
 import cv2 
 import numpy as np 
-from math import cos,sin
+from math import cos,sin,floor
 from hough_lines_corners import HoughLineCornerDetector
 
-from sklearn.cluster import KMeans
+from Contrast import apply_brightness_contrast
 
 
 def getBestContourShape(contours):
@@ -24,11 +24,12 @@ def getBestContourShape(contours):
                
 
 
-image_path = "my_doc_example4.jpg"
+image_path = "My_Doc_Examples/my_doc_example7.jpg"
 
 
-image_height = 1200
-image_width = 600
+
+
+
 
 
 
@@ -37,53 +38,68 @@ closing_kernel =  cv2.getStructuringElement(
             (3, 3)
         )
 
+
+
 canny_lower_threshold = 100
 canny_upper_threshold= 180
 
 
 
 
-
+## Read in image
 image = cv2.imread(image_path)
-image = cv2.resize(image,(image_width,image_height))
 
+initial_height , initial_width , initial_channels = image.shape
+
+
+image_ratio = initial_width/initial_height
+image_height = 1000
+image_width = floor(image_height*image_ratio)
+
+
+## Resize Image 
+image = cv2.resize(image,(image_width,image_height))
 cv2.imshow("image",image)
 
 
-
+## Greyscale
 
 greyscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
 cv2.imshow("greyscale_image",greyscale_image)
 
 
+contrast_image = apply_brightness_contrast(greyscale_image, 0,64)
+cv2.imshow("contrast_image",contrast_image)
+
+
+
+## Blur
 # ## using gaussian, but could use mean denoiser
-blur_image = cv2.GaussianBlur(greyscale_image, (5,5), 1)
+blur_image = cv2.GaussianBlur(contrast_image, (21,21), 1)
 cv2.imshow("blur_image",blur_image)
 
 
-threshold_used, thresholded_image = cv2.threshold(blur_image, 128,255, cv2.THRESH_BINARY )
-
-
+## Threshold Image
+threshold_used, thresholded_image = cv2.threshold(blur_image, 160,255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# thresholded_image = cv2.adaptiveThreshold(blur_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#  cv2.THRESH_BINARY,11,2)
 cv2.imshow("thresholded_image",thresholded_image)
 
-
+## Closing 
 closed_image = cv2.morphologyEx(
         thresholded_image,
         cv2.MORPH_CLOSE,
         closing_kernel,
-        iterations=10
+        iterations=0
     )
-
 cv2.imshow("closed_image",closed_image)
 
 
+## Detect Edges 
 canny_image = cv2.Canny( closed_image,
                          canny_lower_threshold,
                          canny_upper_threshold
                         )
-
 cv2.imshow("canny_image",canny_image)
 
 
@@ -97,25 +113,23 @@ contours_img = image.copy()
 biggest_contours = image.copy()
 
 
+## Find Contours
 contours, hierarchy = cv2.findContours(canny_image,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 cv2.drawContours(contours_img, contours, -1, (0,255,0), 10)
 
 print("contours")
 
+## Display Contour Points
 for points in contours:
 
-        print("SINGLE CONTOUR")
-    # if len(points) ==4:
-        for point in points:
-            print("POINT: ", point)
-            print("---------------------")
-            x = int(point[0][0])
-            y = int(point[0][1])
-            
+   
+    for point in points:
+    
+        x = int(point[0][0])
+        y = int(point[0][1])
         
-            cv2.circle(contours_img, (x,y), radius=20, color=(0, 0, 255), thickness=-1)
-
+    
+        cv2.circle(contours_img, (x,y), radius=20, color=(0, 0, 255), thickness=-1)
 
 cv2.imshow("contours image: ", contours_img)
 
@@ -123,156 +137,52 @@ cv2.imshow("contours image: ", contours_img)
 
 best_contour = getBestContourShape(contours)
 
-cv2.drawContours(biggest_contours, contours, -1, (0,255,0), 10)
+if best_contour is not  None:
+        
+    cv2.drawContours(biggest_contours, [best_contour], -1, (0,255,0), 10)
 
-for point in best_contour:
-   
-            
-    x = int(point[0][0])
-    y = int(point[0][1])
+    for point in best_contour:
     
+                
+        x = int(point[0][0])
+        y = int(point[0][1])
+        
 
-    cv2.circle(biggest_contours, (x,y), radius=20, color=(0, 0, 255), thickness=-1)
+        cv2.circle(biggest_contours, (x,y), radius=20, color=(0, 0, 255), thickness=-1)
 
 
-cv2.imshow("best contours image: ", biggest_contours)
+    cv2.imshow("best contours image: ", biggest_contours)
 
 
 
-ordered_points = HoughLineCornerDetector._order_points(best_contour)
-print("ORDERED POINTS: ", ordered_points)
 
+    ## Warp Image
+    ordered_points = HoughLineCornerDetector._order_points(best_contour)
+    print("ORDERED POINTS: ", ordered_points)
 
-dst = np.array([
-        [0, 0],                         # Top left point
-        [image_width - 1, 0],              # Top right point
-        [image_width - 1, image_height - 1],  # Bottom right point
-        [0, image_height - 1]],            # Bottom left point
-        dtype = "float32"               # Date type
-    )
 
+    dst = np.array([
+            [0, 0],                         # Top left point
+            [image_width - 1, 0],              # Top right point
+            [image_width - 1, image_height - 1],  # Bottom right point
+            [0, image_height - 1]],            # Bottom left point
+            dtype = "float32"               # Date type
+        )
 
-matrix = cv2.getPerspectiveTransform(ordered_points, dst)
-                         
 
-warped_image = cv2.warpPerspective(image, matrix, (image_width, image_height)) 
+    matrix = cv2.getPerspectiveTransform(ordered_points, dst)
+    warped_image = cv2.warpPerspective(image, matrix, (image_width, image_height)) 
 
-cv2.imshow("warped image contours", warped_image)
+    cv2.imshow("warped image contours", warped_image)
 
+else:
+    print("BEST CONTOUR NOT FOUND")
 
 
 
 
 
 
-
-
-
-
-
-
-lines = cv2.HoughLines(
-        canny_image,
-        0.1,
-        0.1,
-        10
-    )
-
-
-lines_image = image.copy()
-lines_image[:,:,:]=0
-
-
-for line in lines:
-    rho = line[0][0]
-    theta = line[0][1]
-    
-
-    a = cos(theta)
-    b = sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    pt1 = (int(x0 + image_width*(-b)), int(y0 + image_height*(a)))
-    pt2 = (int(x0 - image_width*(-b)), int(y0 - image_height*(a)))
-    cv2.line(lines_image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-    
-
-
-
-cv2.imshow("lines_image",lines_image)
-
-
-
-
-
-corner_detector = HoughLineCornerDetector()
-corner_points = corner_detector._get_intersections(lines,image_width,image_height)
-
-
-corners_image = lines_image.copy()
-
-
-for point in corner_points:
-    
-    x = point[0][0]
-    y = point[0][1]
-   
-    if 0<=x<image_width and 0 <= y < image_height: 
-        corners_image[y,x] = [255,0,0]
-    
-
-cv2.imshow("corners_image",corners_image)
-
-
-
-quadrilaterals_points = corner_detector._find_quadrilaterals(corner_points)
-
-
-print("QUADS")
-print( quadrilaterals_points)
-
-quadrilateral_image = lines_image.copy()
-
-
-
-
-for point in quadrilaterals_points:
-    # print(point)
-    x = int(point[0][0])
-    y = int(point[0][1])
-    
-    print("X: ",x)
-    print("Y: ", y)
-    cv2.circle(quadrilateral_image, (x,y), radius=20, color=(0, 255, 0), thickness=-1)
-
-
-
-
-
-cv2.imshow("quadrilateral_image",quadrilateral_image)
-
-print("QUAD POINTS LEN: ", len(quadrilaterals_points))
-rect = HoughLineCornerDetector._order_points(np.asarray(quadrilaterals_points))
-
-
-dst = np.array([
-        [0, 0],                         # Top left point
-        [image_width - 1, 0],              # Top right point
-        [image_width - 1, image_height - 1],  # Bottom right point
-        [0, image_height - 1]],            # Bottom left point
-        dtype = "float32"               # Date type
-    )
-
-
-matrix = cv2.getPerspectiveTransform(rect, dst)
-                         
-
-warped_image = cv2.warpPerspective(image, matrix, (image_width, image_height)) 
-
-cv2.imshow("warped image", warped_image)
-
-
-# print(lines)
 
 
 
